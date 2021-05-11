9f53df06368dd723004f6933abfc4f92ea0425b0
DorianI <>
2021-05-11 22:11:53 +0000

    day to const

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let userInput = 0
-let day = 'date'
+let const = 'date'
 document.getElementById('button').addEventListener('click', museum)
 
 function museum () {
   userInput = document.getElementById('age').value
   userInput = parseInt(userInput)
-  if ((userInput > 12 && userInput < 21) || (day === 'Tuesday' || day === 'Thursday')) {
+  if ((userInput > 12 && userInput < 21) || (const === 'Tuesday' || const === 'Thursday')) {
     document.getElementById('answer').innerHTML = 'You are elegible for student pricing'
   } else {
     document.getElementById('answer').innerHTML = 'You must pay regular pricing'
