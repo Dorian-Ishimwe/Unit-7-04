47766cb9208aaed52e2bfed06e77083f28701985
DorianI <>
2021-05-11 17:15:22 +0000

    fixed style errors

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let userInput = 0
-var day = 'day'
+let day = 'day'
 document.getElementById('button').addEventListener('click', museum)
 
 function museum () {
- userInput = document.getElementById('age').value
- userInput = parseInt(userInput)
- 
+  userInput = document.getElementById('age').value
+  userInput = parseInt(userInput)
  if ((userInput > 12 && userInput < 21) || (day == 'Tuesday' || day == 'Thursday')) {
-   document.getElementById('answer').innerHTML = 'You are elegible for student pricing'
- } else {
-   document.getElementById('answer').innerHTML = 'You must pay regular pricing'
- }
-}
+    document.getElementById('answer').innerHTML === 'You are elegible for student pricing'
+  } else {
+    document.getElementById('answer').innerHTML === 'You must pay regular pricing'
+  }
+} 
