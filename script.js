2bf8002c9a56c4b286a052a9b291a47f36591c44
DorianI <>
2021-05-11 16:55:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let userInput = 0
+var day = 'day'
+document.getElementById('button').addEventListener('click', museum)
+
+function museum () {
+ userInput = document.getElementById('age').value
+ userInput = parseInt(userInput)
+ 
+ if ((userInput > 12 && userInput < 21) || (day == 'Tuesday' || day == 'Thursday')) {
+   document.getElementById('answer').innerHTML = 'You are elegible for student pricing'
+ } else {
+   document.getElementById('answer').innerHTML = 'You must pay regular pricing'
+ }
+}
