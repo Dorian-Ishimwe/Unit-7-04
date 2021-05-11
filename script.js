c5942828e127fdbd5307abe9a3c3c15516181419
DorianI <>
2021-05-11 17:38:20 +0000

    more fixes to js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ document.getElementById('button').addEventListener('click', museum)
 function museum () {
   userInput = document.getElementById('age').value
   userInput = parseInt(userInput)
- if ((userInput > 12 && userInput < 21) || (day == 'Tuesday' || day == 'Thursday')) {
-    document.getElementById('answer').innerHTML === 'You are elegible for student pricing'
+  if ((userInput > 12 && userInput < 21) || (day === 'Tuesday' || day === 'Thursday')) {
+    document.getElementById('answer').innerHTML = 'You are elegible for student pricing'
   } else {
-    document.getElementById('answer').innerHTML === 'You must pay regular pricing'
+    document.getElementById('answer').innerHTML = 'You must pay regular pricing'
   }
-} 
+}
